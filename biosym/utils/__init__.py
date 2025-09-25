65219531bd9dcc3b9a4084cec1a0ada76d52c6a0
tbalo <theodoros.balougias@fau.de>
2025-09-25 10:44:37 +0200

    Find marker positions during standing(first samples of trc file) and plot their positions

--- biosym/utils/__init__.py
+++ biosym/utils/__init__.py
@@ -1,3 +1,4 @@
 from .read_write_files import read_mot, read_trc, write_mot
 from .segment_gait_cycles import segment_gait_averages
+from .standing_markers import create_standing_markers, plot_standing_markers_xy
 from .states import *
